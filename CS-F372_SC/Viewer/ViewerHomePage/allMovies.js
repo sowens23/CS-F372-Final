2e8504f1cf4534ecf5d87ac2e4411d0807b9d637
IvySwenson <xswenson@alaska.edu>
2025-04-10 12:19:54 -0800

    Update allMovies.js

--- CS-F372_SC/Viewer/ViewerHomePage/allMovies.js
+++ CS-F372_SC/Viewer/ViewerHomePage/allMovies.js
@@ -1,13 +1,152 @@
 const allMovies = {
-  "harry1": { title: "Harry Potter", genre: "Action", poster: "/HomeTemplate/MoviePosters/posters/harrypotter.webp" },
-  "Coffee": { title: "A complete Unknown", genre: "History", poster: "/HomeTemplate/MoviePosters/posters/a-complete-unknown_7hsvuwdd_500x749.webp" },
-  "Cow": { title: "Minecraft Movie", genre: "Adventure", poster: "/HomeTemplate/MoviePosters/posters/a-minecraft-movie_nccb8hda_500x749.webp" },
-  "DesertLandover": { title: "The wild Robot", genre: "Family/Sci-fi", poster: "/HomeTemplate/MoviePosters/posters/wild_robot_ver2_500x749.webp" },
-  "ForestFallTime": { title: "Paddington in Peru", genre: "Faimly/Sci-fi", poster: "/HomeTemplate/MoviePosters/posters/aib4j8oeERZgjrG0fw1xjevTGyM_500x749.webp" },
-  "ForsythiaBlossom": { title: "Becoming Led Zeppelin", genre: "Documentary/Musical", poster: "/HomeTemplate/MoviePosters/posters/becoming-led-zeppelin_9ecrpjky_500x749.webp" },
-  "FruitPie": { title: "Black Bag", genre: "Spy thriller", poster: "/HomeTemplate/MoviePosters/posters/black-bag_ntykezqf_500x749.webp" },
-  "JapanPagoda": { title: "Conclave", genre: "Thriller/Mystery", poster: "/HomeTemplate/MoviePosters/posters/conclave_500x749.webp" },
-  "LavenderField": { title: "Pirates of the Caribbean", genre: "Adventure/Action", poster: "/HomeTemplate/MoviePosters/posters/debe2de5e18176e89367ad27e36394be_d040a0c9-62db-4ad0-8487-eeb79c3a43b6_500x749.webp" },
-  "OceanTurtle": { title: "Snow White", genre: "Family/Fantasy", poster: "/HomeTemplate/MoviePosters/posters/disneys-snow-white_buebkjyp_500x749.webp" },
-  "swan": { title: "Dune", genre: "Sci-fi/Adventure", poster: "/HomeTemplate/MoviePosters/posters/dune_1biov8fc_500x749.webp" },
+  "harry1": {
+    title: "Harry Potter",
+    genre: "Action",
+    poster: "/HomeTemplate/MoviePosters/posters/harrypotter.webp"
+  },
+  "Coffee": {
+    title: "A complete Unknown",
+    genre: "History",
+    poster: "/HomeTemplate/MoviePosters/posters/a-complete-unknown_7hsvuwdd_500x749.webp"
+  },
+  "Cow": {
+    title: "Minecraft Movie",
+    genre: "Adventure",
+    poster: "/HomeTemplate/MoviePosters/posters/a-minecraft-movie_nccb8hda_500x749.webp"
+  },
+  "DesertLandover": {
+    title: "The wild Robot",
+    genre: "Family/Sci-fi",
+    poster: "/HomeTemplate/MoviePosters/posters/wild_robot_ver2_500x749.webp"
+  },
+  "ForestFallTime": {
+    title: "Paddington in Peru",
+    genre: "Faimly/Sci-fi",
+    poster: "/HomeTemplate/MoviePosters/posters/aib4j8oeERZgjrG0fw1xjevTGyM_500x749.webp"
+  },
+  "ForsythiaBlossom": {
+    title: "Becoming Led Zeppelin",
+    genre: "Documentary/Musical",
+    poster: "/HomeTemplate/MoviePosters/posters/becoming-led-zeppelin_9ecrpjky_500x749.webp"
+  },
+  "FruitPie": {
+    title: "Black Bag",
+    genre: "Spy thriller",
+    poster: "/HomeTemplate/MoviePosters/posters/black-bag_ntykezqf_500x749.webp"
+  },
+  "JapanPagoda": {
+    title: "Conclave",
+    genre: "Thriller/Mystery",
+    poster: "/HomeTemplate/MoviePosters/posters/conclave_500x749.webp"
+  },
+  "LavenderField": {
+    title: "Pirates of the Caribbean",
+    genre: "Adventure/Action",
+    poster: "/HomeTemplate/MoviePosters/posters/debe2de5e18176e89367ad27e36394be_d040a0c9-62db-4ad0-8487-eeb79c3a43b6_500x749.webp"
+  },
+  "OceanTurtle": {
+    title: "Snow White",
+    genre: "Family/Fantasy",
+    poster: "/HomeTemplate/MoviePosters/posters/disneys-snow-white_buebkjyp_500x749.webp"
+  },
+  "swan": {
+    title: "Dune",
+    genre: "Sci-fi/Adventure",
+    poster: "/HomeTemplate/MoviePosters/posters/dune_1biov8fc_500x749.webp"
+  },
+  "Freakier": {
+    title: "Freakier Friday",
+    genre: "Family",
+    poster: "/HomeTemplate/MoviePosters/posters/freakier-friday_n5jadnjf_500x749.webp"
+  },
+  "Gladiator": {
+    title: "Gladiator",
+    genre: "Action/Adventure",
+    poster: "/HomeTemplate/MoviePosters/posters/gladiator_ii_500x749.webp"
+  },
+  "how to train your dragon": {
+    title: "How To Train Your Dragon",
+    genre: "Fantasy/Action",
+    poster: "/HomeTemplate/MoviePosters/posters/how-to-train-your-dragon_z9xd7rrd_480x.progressive.webp"
+  },
+  "Interstellar": {
+    title: "Interstellar",
+    genre: "Sci-fi/Adventure",
+    poster: "/HomeTemplate/MoviePosters/posters/interstellar_593eaeff_500x749.jpg"
+  },
+  "Jurassic": {
+    title: "Jurassic Wolrd",
+    genre: "Adventure/Sci-fi",
+    poster: "/HomeTemplate/MoviePosters/posters/jurassic-world-rebirth_6gyim32q_500x749.webp"
+  },
+  "LioandStitch": {
+    title: "Lio & Stitch",
+    genre: "Sci-fi/Adventure",
+    poster: "/HomeTemplate/MoviePosters/posters/lilo-stitch_fykrzeq9_500x749.webp"
+  },
+  "Mickey17": {
+    title: "Mickey 17",
+    genre: "Sci-fi/Thriller",
+    poster: "/HomeTemplate/MoviePosters/posters/mickey-17_bjfndnr1_500x749.webp"
+  },
+  "missionimpossible": {
+    title: "Mission Impossible",
+    genre: "Action/Thriller",
+    poster: "/HomeTemplate/MoviePosters/posters/mission_impossible__the_final_reckoning_500x749.jpg"
+  },
+  "NickelBoys": {
+    title: "Nickel Boys",
+    genre: "Historical drama",
+    poster: "/HomeTemplate/MoviePosters/posters/nickel_boys_500x749.webp"
+  },
+  "Novocaine": {
+    title: "Novocaine",
+    genre: "Action",
+    poster: "/HomeTemplate/MoviePosters/posters/novocaine_500x749.webp"
+  },
+  "lionking": {
+    title: "MUFASA The Lion King",
+    genre: "Adventure/Musical",
+    poster: "/HomeTemplate/MoviePosters/posters/scan003_15ae586d-23aa-4de9-990b-ac5cdc4f98ef_500x749.webp"
+  },
+  "dogman": {
+    title: "Dog Man",
+    genre: "Family",
+    poster: "/HomeTemplate/MoviePosters/posters/scan005_4629af5e-eca6-4ea5-a656-a8252a426b29_500x749.webp"
+  },
+  "duneone": {
+    title: "Part One: Dune",
+    genre: "Sci-fi/Adventure",
+    poster: "/HomeTemplate/MoviePosters/posters/scan_2d710c1d-40df-446d-9f2b-c685a95089e0_500x749.webp"
+  },
+  "sonic": {
+    title: "Sonic the Hedgehog 3",
+    genre: "Action/Adventure",
+    poster: "/HomeTemplate/MoviePosters/posters/sonic-the-hedgehog-3_ncwdwcum_500x749.webp"
+  },
+  "superman": {
+    title: "Superman Returns",
+    genre: "Action/Sci-fi",
+    poster: "/HomeTemplate/MoviePosters/posters/superman_returns_8b82f995-1a7e-46ec-b979-ca72daa8e518_500x749.webp"
+  },
+  "altoknights": {
+    title: "The Alto Knights",
+    genre: "Thriller/Crime",
+    poster: "/HomeTemplate/MoviePosters/posters/the-alto-knights_tb39opez_500x749.webp"
+  },
+  "themenu": {
+    title: "The Menu",
+    genre: "Horror/Mystery",
+    poster: "/HomeTemplate/MoviePosters/posters/the-menu_tepa1mmt_500x749.webp"
+  },
+  "Outrun": {
+    title: "The Outrun",
+    genre: "Drama",
+    poster: "/HomeTemplate/MoviePosters/posters/the-outrun_tody9dkk_500x749.webp"
+  },
+  "Thunderbolts": {
+    title: "Thunderbolts*",
+    genre: "Action/Sci-fi",
+    poster: "/HomeTemplate/MoviePosters/posters/thunderbolts_ver2_500x749.webp"
+  }
 };
